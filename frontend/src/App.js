95ed89bb3cf6f91df6cc4392bebc90e5a6cdf52e
Besito <adrianahernandez109@gmail.com>
2025-01-12 20:09:09 -0500

    add jwt auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import './App.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const calendarEl = document.getElementById('calendar');
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -13,11 +10,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
 
 // export default App;
 $('#calendar').fullCalendar({
